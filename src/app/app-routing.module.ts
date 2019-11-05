f93b5aae029f6de53c2a502bd92202c7c97ec80a
quangpt <quangpt@ominext.com>
2019-11-05 17:29:21 +0700

    add style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-// import {Page01Component} from './pages/page01/page01.component';
+import {Page01Component} from './pages/page01/page01.component';
 // import {Page02Component} from './pages/page02/page02.component';
 // import {Page03Component} from './pages/page03/page03.component';
 // import {Page04Component} from './pages/page04/page04.component';
 // import {Page05Component} from './pages/page05/page05.component';
 const routes: Routes = [
-//   {
-//     path: 'page01',
-//     component: Page01Component
-//   },
+  {
+    path: 'index',
+    component: Page01Component
+  }
 //   {
 //     path: 'page02',
 //     component: Page02Component
