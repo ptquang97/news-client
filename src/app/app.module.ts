bc32d54426c22325f6bd70b42c1aec4867d5be90
quangpt <quangpt@ominext.com>
2019-11-05 17:08:13 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClient , HttpClientModule} from '@angular/common/http';
+import { LSelect2Module } from 'ngx-select2';
+import { Page01Component } from './pages/page01/page01.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    Page01Component
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LSelect2Module
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
